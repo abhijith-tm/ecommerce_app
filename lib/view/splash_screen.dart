a8d00f4e2a9c2999b075b58dfd9fe2468bc859d0
abhijith-tm <abhijithtm20@gmail.com>
2025-05-30 11:12:53 +0530

    first commit

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_app/view/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+          child: Text(
+        "ZOYOTO",
+        style: GoogleFonts.bebasNeue(fontSize: 50, color: Colors.white),
+      )),
+    );
+  }
+}
